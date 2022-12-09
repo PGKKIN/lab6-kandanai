43e131e1079b6c54477ca5211e1a2edb506d7323
PGKKIN <kandanaik@gmail.com>
2022-12-09 16:11:48 +0700

    ab6_1.cpp completed

--- lab6_1.cpp
+++ lab6_1.cpp
@@ -1,9 +1,30 @@
-#include<iostream>
-using namespace std;
+#include <iostream>
 
-int main(){
-    cout << "Enter an integer: ";
-    cout << "#Even numbers = ";
-    cout << "#Odd numbers = ";
+using namespace std;
+int n=1;
+int y=0;
+int z=0;
+int main()
+{
+
+    while (n!=0)
+    {
+        cout<<"Enter an integer: ";
+        cin>>n;
+        n=n;
+        if(n%2==0)
+        {
+            y+=1;
+        }
+        else
+        {
+            z+=1;
+        }
+
+    }
+    cout<<"#Even numbers = ";
+    cout<<y-1<<"\n";
+    cout<<"#Odd numbers = ";
+    cout<<z;
     return 0;
 }
